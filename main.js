1e3da8c3c1da04eb78c1de6ce597a50233db32a3
anis-hannachi <hannachi.anis@gmail.com>
2021-04-12 22:12:08 +0100

    finished

--- main.js
+++ main.js
@@ -31,7 +31,24 @@ function showQuestion(){
         x.style.display = "block";
     }
 }
-
+function showQuestion1(){
+    $(".cat").hide();
+    $("#titel").hide();
+    $("#image").hide();
+    var x = document.getElementById("question11");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    }
+}
+function showQuestion2(){
+    $(".cat").hide();
+    $("#titel").hide();
+    $("#image").hide();
+    var x = document.getElementById("question12");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    }
+}
 function validate() {
         for(var i = 0; i < checkboxes.length; i++) {
             if(checkboxes[i].value === "right" && checkboxes[i].checked === true) {
@@ -46,8 +63,12 @@ function validate() {
     
 var movies = [$("#question1"),$("#question2"),$("#question3"),$("#question4"),$("#question5")];
 var questions = ["question1","question2","question3","question4","question5"];
+var pokemons = [$("#question11"),$("#question21"),$("#question31"),$("#question41"),$("#question51")];
+var pokeQuestions = ["question11","question21","question31","question41","question51"];
+var singers = [$("#question12"),$("#question22"),$("#question32"),$("#question42"),$("#question52")]
+var singerQuestions = ["question12","question22","question32","question42","question52"];
 var counter = 0;
-var idss = ["a","b","c","d","e","f","j","k","l","m","n","o","p","q","r","s","t"];
+
 function nextQuestion(){    
     for(var i=0; i<=checkboxes.length-1; i++){
       if(checkboxes[i].checked === true){ 
@@ -65,11 +86,52 @@ function nextQuestion(){
     } 
     }  
      if(!test){
-         alert("please answer the question !!!")
+         alert("Please check before clicking Next !!!")
         }
         test = false
 }
-
+function nextQuestion1(){    
+    for(var i=0; i<=checkboxes.length-1; i++){
+      if(checkboxes[i].checked === true){ 
+        $(".red").css("color","black");
+        $(".green").css("color","black"); 
+       test = true
+       checkboxes[i].checked = false
+       pokemons[counter].hide();
+        counter ++ ;  
+        var x = document.getElementById(pokeQuestions[counter]);
+        if (x.style.display === "none"){
+        x.style.display = "block";
+    }
+   
+    } 
+    }  
+     if(!test){
+         alert("Please check before clicking Next !!!")
+        }
+        test = false
+}
+function nextQuestion2(){    
+    for(var i=0; i<=checkboxes.length-1; i++){
+      if(checkboxes[i].checked === true){ 
+        $(".red").css("color","black");
+        $(".green").css("color","black"); 
+       test = true
+       checkboxes[i].checked = false
+       singers[counter].hide();
+        counter ++ ;  
+        var x = document.getElementById(singerQuestions[counter]);
+        if (x.style.display === "none"){
+        x.style.display = "block";
+    }
+   
+    } 
+    }  
+     if(!test){
+         alert("Please check before clicking Next !!!")
+        }
+        test = false
+}
 function score(){
     $("body").innerHTML = "";
     var x = document.getElementById("score");
@@ -78,7 +140,22 @@ function score(){
     }
     $(".allQuestions").html("").append("<h1>"+ name1+ " your score is " + count + " %" +"</h1>")
 }
-
+function score1(){
+    $("body").innerHTML = "";
+    var x = document.getElementById("score1");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    }
+    $(".allPokemon").html("").append("<h1>"+ name1+ " your score is " + count + " %" +"</h1>")
+}
+function score2(){
+    $("body").innerHTML = "";
+    var x = document.getElementById("score2");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    }
+    $(".allSinger").html("").append("<h1>"+ name1+ " your score is " + count + " %" +"</h1>")
+}
 function exit(){
     location.reload()
 }
