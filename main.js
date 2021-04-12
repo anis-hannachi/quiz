c253939385fa956a22722a1efd703831e78933d7
anis-hannachi <hannachi.anis@gmail.com>
2021-04-12 15:16:31 +0100

    refactored

--- main.js
+++ main.js
@@ -1,78 +1,82 @@
-function each(coll, func) { 
-    if (Array.isArray(coll)) { 
-          for (var i = 0; i < coll.length; i++) { 
-                func(coll[i], i); 
-          } 
-    } else { 
-          for (var key in coll) { 
-                func(coll[key], key); 
-          } 
-    } 
-}
-function map(coll,f){
-	var acc=[];
-	if(!Array.isArray(coll)){
-		acc={};
-	}
-	each(coll,function(element,key){
-		acc[key]=f(element,key);
-	});
-	return acc;
-}
-function filter(collection,predicate){
-    var acc=[];
-    if (!Array.isArray(collection)){
-        acc = {};
-    }
-      each(collection,function(element,key){
-          if(predicate(element,key)){
-              if(Array.isArray(collection)){
-                  acc.push(element)
-              }
-              else{
-                  acc[key] = element ;
-              }
-          }
-      });
-    return acc;
-  }
-var rightAnswersMovies = {
-    question 1 : ,
-    question 2 : ,
-    question 3 : ,
-    question 4 : ,
-    question 5 : 
-}
-var rightAnswersAnimals = {
-    question 1 : ,
-    question 2 : ,
-    question 3 : ,
-    question 4 : ,
-    question 5 :  
-}
-var rightAnswersPokemon = {
-    question 1 : ,
-    question 2 : ,
-    question 3 : ,
-    question 4 : ,
-    question 5 : 
-}
-var rightAnswersSingerName = {
-    question 1 : ,
-    question 2 : ,
-    question 3 : ,
-    question 4 : ,
-    question 5 : 
-}
-var categorys = ["Movies","Animals","Pokemon","Singer Name"];
-var userName = document.getElementById("#user");
-var answer = document.getElementsByClassName(".answer")
-function makeUser(userName){
-    return {
-        userName : userName 
-    }
-}
-function getAnswer(answer){
-    var answers = {};
+var cate="";
+var nbq=1;
+var name1="";
 
+function test(va){
+    cate=va;
+    alert (cate);
+}
+
+var categorys = ["Movies","Pokemon","Singer Name"];
+
+
+
+function show() {
+    name1= document.getElementById("user").value;
+    $(".user").hide();
+    var x = document.getElementById("cat");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+}
+function showQuestion(){
+    $(".cat").hide();
+    $("#titel").hide();
+    $("#image").hide();
+    var x = document.getElementById("question1");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    }
+}
+
+function validate() {
+    var checkboxes = $(".check input");
+        for(var i = 0; i < checkboxes.length; i++) {
+            if(checkboxes[i].value === "right" && checkboxes[i].checked === true) {
+              $(".green").css("color","green");
+            }
+            else if (checkboxes[i].value === "wrong" && checkboxes[i].checked === true){
+                $(".red").css("color","red");
+            }
+        }
+}
+
+var movies = [$("#question1"),$("#question2"),$("#question3"),$("#question4"),$("#question5")];
+var questions = ["question1","question2","question3","question4","question5"];
+var counter = 0;
+var idss = ["a","b","c","d","e","f","j","k","l","m","n","o","p","q","r","s","t"];
+function nextQuestion(){
+    for(var i=0; i<=idss.length-1; i++){
+      if($(".check #idss[i]").checked === true){
+        movies[counter].hide();
+        counter ++ ;
+        var x = document.getElementById(questions[counter]);
+        if (x.style.display === "none"){
+        x.style.display = "block";
+    }
+    $(".red").css("color","black");
+    $(".green").css("color","black");
+    return true
+    }
+    else {
+    alert("Check First");
+    return false
+    }  
+    }   
+}
+// count = 0;
+// function score(){
+//     var x = document.getElementById("score");
+//     if (x.style.display === "none"){
+//         x.style.display = "block";
+//         $("#score").append("<p class="msg"></p>);
+//     }
+//     if ($(".check input").value === "right"){
+//         count +=20;
+//     }
+//     return name1 + " your score is " + count + " %" 
+// }
+
+function exit(){
+    location.reload()
 }
