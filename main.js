8b64506fdf5375e7e9d9fe92ba7e959d6fd37d55
anis-hannachi <hannachi.anis@gmail.com>
2021-04-12 16:50:45 +0100

    first category works

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var cate="";
 var nbq=1;
 var name1="";
+var test = false
+var checkboxes = $(".check input");
+var count = 0;
 
 function test(va){
     cate=va;
@@ -30,52 +33,51 @@ function showQuestion(){
 }
 
 function validate() {
-    var checkboxes = $(".check input");
         for(var i = 0; i < checkboxes.length; i++) {
             if(checkboxes[i].value === "right" && checkboxes[i].checked === true) {
               $(".green").css("color","green");
+              count+=20;
             }
             else if (checkboxes[i].value === "wrong" && checkboxes[i].checked === true){
                 $(".red").css("color","red");
             }
         }
-}
-
+    }
+    
 var movies = [$("#question1"),$("#question2"),$("#question3"),$("#question4"),$("#question5")];
 var questions = ["question1","question2","question3","question4","question5"];
 var counter = 0;
 var idss = ["a","b","c","d","e","f","j","k","l","m","n","o","p","q","r","s","t"];
-function nextQuestion(){
-    for(var i=0; i<=idss.length-1; i++){
-      if($(".check #idss[i]").checked === true){
-        movies[counter].hide();
-        counter ++ ;
+function nextQuestion(){    
+    for(var i=0; i<=checkboxes.length-1; i++){
+      if(checkboxes[i].checked === true){ 
+        $(".red").css("color","black");
+        $(".green").css("color","black"); 
+       test = true
+       checkboxes[i].checked = false
+       movies[counter].hide();
+        counter ++ ;  
         var x = document.getElementById(questions[counter]);
         if (x.style.display === "none"){
         x.style.display = "block";
     }
-    $(".red").css("color","black");
-    $(".green").css("color","black");
-    return true
-    }
-    else {
-    alert("Check First");
-    return false
+   
+    } 
     }  
-    }   
+     if(!test){
+         alert("please answer the question !!!")
+        }
+        test = false
 }
-// count = 0;
-// function score(){
-//     var x = document.getElementById("score");
-//     if (x.style.display === "none"){
-//         x.style.display = "block";
-//         $("#score").append("<p class="msg"></p>);
-//     }
-//     if ($(".check input").value === "right"){
-//         count +=20;
-//     }
-//     return name1 + " your score is " + count + " %" 
-// }
+
+function score(){
+    $("body").innerHTML = "";
+    var x = document.getElementById("score");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    }
+    $(".allQuestions").html("").append("<h1>"+ name1+ " your score is " + count + " %" +"</h1>")
+}
 
 function exit(){
     location.reload()
